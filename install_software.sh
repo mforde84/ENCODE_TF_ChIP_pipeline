9cba7926d39f1745dffa7b220ea8c1d729cea832
Martin Forde <mforde84@gmail.com>
2016-09-01 22:19:28 -0500

    Update install_software.sh

--- install_software.sh
+++ install_software.sh
@@ -6,7 +6,6 @@ sudo apt-get install build-essential libboost-all-dev git zlib1g-dev libcairo2-d
 mkdir ucsc ;
 cd ucsc;
 wget -m -e robots=off --no-parent http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/;
-cd hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64;
 rm -rf hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/index* hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/blat;
 for f in hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/*; do 
  chmod +x $f; 
