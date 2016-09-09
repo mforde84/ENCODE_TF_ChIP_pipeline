38e2d7519f220d535be0d29a72338a36867fdec9
Martin Forde <mforde84@gmail.com>
2016-09-09 11:08:59 -0500

    scripting is validated and runs without error - proper scope escaping for variables

--- run.sh
+++ run.sh
@@ -18,21 +18,21 @@ mkdir "$out_dir";
 #alignments and sam file generation - multithreaded aln, samse is memory heavy
 for f in "$reads_dir"/*.fastq; do
  temp_loc=$(echo $f | sed "s~$reads_dir\/~~g"); 
- bwa aln -I -B 0 -t $threads $genome $f | bwa samse $genome - $f > encode_temp_dir/"$temp_loc".sam;
+ bwa aln -I -B 0 -t "$threads" "$genome" "$f" | bwa samse "$genome" - "$f" > encode_temp_dir/"$temp_loc".sam;
 done;
 
 #q30 filter - single core
-find encode_temp_dir/ -name "*sam" | xargs -n 1 -P $threads -iFILES bash -c '
+find encode_temp_dir/ -name "*sam" | xargs -n 1 -P "$threads" -iFILES bash -c '
  samtools view -F 1548 -h -S -q 30 -o FILES_q30.sam FILES;
 ';
 
 #generate tag densities for q30 - single core
-find encode_temp_dir/ -name "*_q30.sam" | xargs -n 1 -P $threads -iFILES bash -c '
+find encode_temp_dir/ -name "*_q30.sam" | xargs -n 1 -P "$threads" -iFILES bash -c '
  samtools view -Sb FILES | bedtools bamtobed -i stdin | awk '\''BEGIN{FS="\t";OFS="\t"}{$4="N";print$0}'\'' | gzip -c > FILES.tagAlign.gz;
 ';
 
 #generate bam and wig - single core
-find encode_temp_dir/ -name "*_q30.sam" | xargs -n 1 -P $threads -iFILES bash -c '
+find encode_temp_dir/ -name "*_q30.sam" | xargs -n 1 -P "$threads" -iFILES bash -c '
  out="$out_dir"/$(echo FILES | sed -e "s/encode_temp_dir.//g" | sed "s/\.sam//g")
  samtools view -Sbh FILES > FILES.bam;
  samtools sort FILES.bam "$out".sort;
@@ -44,20 +44,20 @@ find encode_temp_dir/ -name "*_q30.sam" | xargs -n 1 -P $threads -iFILES bash -c
 ';
 
 #generate pooled tags from biological replicates - single core
-find encode_temp_dir/ -name "*rep1*.tagAlign.gz" | xargs -n 1 -P $threads -iFILES bash -c '
+find encode_temp_dir/ -name "*rep1*.tagAlign.gz" | xargs -n 1 -P "$threads" -iFILES bash -c '
  pool=$(echo FILES | sed "s/rep1/rep0/");
  stringer="";
- for ((x=1; x<=$reps; x++)); do
+ for ((x=1; x<="$reps"; x++)); do
   rep=$(echo FILES | sed "s/rep1/rep$x/g");
   stringer+="$rep"" ";
  done;
- zcat $stringer | gzip -c > $pool;
+ zcat "$stringer" | gzip -c > "$pool";
 ';
 
 #shuffle tags and generate psuedo replicates - single core
 find encode_temp_dir/ -name "*tagAlign.gz"  -not -name "*input*" | xargs -n 1 -P $threads -iFILES bash -c '
  nline=$(( ($(zcat FILES | wc -l) + 1) / 2 )); 
- zcat FILES | shuf | split -d -l $nline - ./FILES.0; 
+ zcat FILES | shuf | split -d -l "$nline" - ./FILES.0; 
  gzip ./FILES.000 &
  gzip ./FILES.001;
  mv ./FILES.000.gz ./FILES.pr1.gz & 
@@ -77,7 +77,7 @@ find encode_temp_dir/ -name "*tagAlign*gz"  -not -name "*input*" | xargs -n 1 -P
  inp=$(echo FILES | sed "s/rep./input\.rep0/" | sed "s/.pr[1-2].gz//g"); 
  base=$(echo FILES | sed "s/^.*\///g" | sed "s/.fastq.sam_q30.sam.tagAlign.gz//g" | sed "s/\.gz//g");
  ss=$(cat FILES.cc | cut -f3 | head -n 1 | cut -d "," -f1); 
- macs2 callpeak -t FILES -c $inp -g $gsize --to-large --nomodel --extsize "$ss" -p 0.1 -n "$base" --outdir encode_temp_dir/ --tempdir encode_temp_dir/;
+ macs2 callpeak -t FILES -c "$inp" -g "$gsize" --to-large --nomodel --extsize "$ss" -p 0.1 -n "$base" --outdir encode_temp_dir/ --tempdir encode_temp_dir/;
 ';
 
 # sort MACS2 peaks - single core
@@ -96,18 +96,18 @@ find encode_temp_dir/ -name "*rep1*Peak*gz" -not -name "*\.pr[1-2]\.*" | xargs -
  if [[ FILES =~ .*narrow.* ]]; then 
   sig="p.value"; 
  fi;
- for ((x=0; x<=$reps; x++)); do
+ for ((x=0; x<="$reps"; x++)); do
   pr1=$(echo FILES | sed "s/rep1\./rep$x\.pr1\./g");
   pr2=$(echo FILES | sed "s/rep1\./rep$x\.pr2\./g");
-  base_pr2=$(echo $pr2 | sed "s/^.*\///g");
+  base_pr2=$(echo "$pr2" | sed "s/^.*\///g");
   echo -ne "$pr1\t$pr2\t$base_pr2\t$sig\n" >> encode_temp_dir/comparisons.txt;
-  if [ $x -lt $reps ] && [ $x -gt 0 ]; then
-   z=$(( $x + 1 ));
-   for ((y=$z; y<=$reps; y++)); do
+  if [ "$x" -lt "$reps" ] && [ "$x" -gt 0 ]; then
+   z=$(( "$x" + 1 ));
+   for ((y="$z"; y<="$reps"; y++)); do
     f1=$(echo FILES | sed "s/rep1/rep$x/g");
     f2=$(echo FILES | sed "s/rep1/rep$y/g"); 
-    base_f1=$(echo $f1 | sed "s/^.*\///g"); 
-    base_f2=$(echo $f2 | sed "s/^.*\///g"); 
+    base_f1=$(echo "$f1" | sed "s/^.*\///g"); 
+    base_f2=$(echo "$f2" | sed "s/^.*\///g"); 
     stringer+="$base_f1""_VS_""$base_f2""~~~";
     echo -ne "$f1\t$f2\t$base_f2\t$sig\n" >> encode_temp_dir/comparisons.txt;
    done;
@@ -142,7 +142,7 @@ find . -name "*rep0*pr1*VS*rep0*pr2*overlap*" | xargs -n 1 -P $threads -iFILES b
  base=$(echo $FILES | sed "s/.rep0.*//g" | sed "s/^.*\///g");
  for f in "$base"*"$type"Peak*overlap*; do
   if [[ "$f" != "$hold" ]]; then 
-   comp=$(wc -l $f | cut -d " " -f 1);
+   comp=$(wc -l "$f" | cut -d " " -f 1);
    if [ "$comp" -gt "$optimal" ]; then
     optimal=$comp;	
    fi;
